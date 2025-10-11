76e1bf34c4c35f2811549928f835be78e365571e
Drago <farghawiloay99@gmail.com>
2025-10-11 22:38:00 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -42,3 +42,18 @@ function type() {
 }
 
 type();
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+}, { threshold: 0.2 });
+
+document.querySelectorAll(".fade-section").forEach(section => {
+  observer.observe(section);
+});
